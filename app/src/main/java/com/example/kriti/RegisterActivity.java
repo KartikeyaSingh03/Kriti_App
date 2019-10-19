7c62c33a23daec540d1ca9de14f370483035392f
Kartikeya Singh <kartikeya.singh030600@gmail.com>
2019-10-19 17:07:47 +0530

    Modified Intent after registration

--- app/src/main/java/com/example/kriti/RegisterActivity.java
+++ app/src/main/java/com/example/kriti/RegisterActivity.java
@@ -83,7 +83,7 @@ public class RegisterActivity extends AppCompatActivity  {
                                         Students s = new Students(name,roll);
                                         root.child("Users").child(currentuser).setValue(s);
                                         finish();
-                                        startActivity(new Intent(getApplicationContext(), ProfilePage.class));
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
                                     }else{
                                         //display some message here
                                         Toast.makeText(RegisterActivity.this,"Webmail ID is already registered",Toast.LENGTH_LONG).show();
