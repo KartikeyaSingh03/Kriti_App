ce7ec2f7b873157204a7284242f7c790a3124063
JatinSH <goeljatin28@gmail.com>
2019-10-19 13:14:43 +0530

    Added two buttons to go to new activities

--- app/src/main/java/com/example/kriti/MainActivity.java
+++ app/src/main/java/com/example/kriti/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.kriti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Courses(View view){
+        Intent intentPost=new Intent(MainActivity.this,CoursesActivity.class);
+        startActivity(intentPost);
+    }
+
+    public void readMaterial(View view){
+        Intent intentPost=new Intent(MainActivity.this,ReadingMaterialActivity.class);
+        startActivity(intentPost);
     }
 }
