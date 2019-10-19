e95e27910fddefbcf0fbc5714d73e127a536cecc
JatinSH <goeljatin28@gmail.com>
2019-10-19 14:08:34 +0530

    Replaced onClick methods with onClickListener

--- app/src/main/java/com/example/kriti/MainActivity.java
+++ app/src/main/java/com/example/kriti/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button courses = (Button) findViewById(R.id.coursesButton) ;
+        courses.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentPost=new Intent(MainActivity.this,CoursesActivity.class);
+                startActivity(intentPost);
+
+            }
+
+        });
+
+        Button material = (Button) findViewById(R.id.readingMaterial) ;
+        material.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentPost=new Intent(MainActivity.this,ReadingMaterialActivity.class);
+                startActivity(intentPost);
+
+            }
+
+        });
     }
 
-    public void Courses(View view){
-        Intent intentPost=new Intent(MainActivity.this,CoursesActivity.class);
-        startActivity(intentPost);
-    }
 
-    public void readMaterial(View view){
-        Intent intentPost=new Intent(MainActivity.this,ReadingMaterialActivity.class);
-        startActivity(intentPost);
-    }
 }
--- app/src/main/java/com/example/kriti/SignInActivity.java
+++ app/src/main/java/com/example/kriti/SignInActivity.java
@@ -3,19 +3,37 @@ package com.example.kriti;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public  class SignInActivity extends AppCompatActivity {
+public  class SignInActivity extends AppCompatActivity  {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+
+        Button register = (Button) findViewById(R.id.registerButton);
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intentPost=new Intent(SignInActivity.this,RegisterActivity.class);
+                startActivity(intentPost);
+
+            }
+
+        });
+
     }
 
-    public void registerButton(View view){
-        Intent intentPost=new Intent(SignInActivity.this,RegisterActivity.class);
-        startActivity(intentPost);
-    }
+//    public void registerButton(View view){
+//        Intent intentPost=new Intent(SignInActivity.this,RegisterActivity.class);
+//        startActivity(intentPost);
+//    }
+
+
+
 }
