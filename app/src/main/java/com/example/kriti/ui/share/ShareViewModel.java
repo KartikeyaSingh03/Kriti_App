309f6eb9234124d318c0914cb4e51eb4e56a9e7e
JatinSH <goeljatin28@gmail.com>
2019-10-19 23:18:13 +0530

    logout and profile fragment

--- app/src/main/java/com/example/kriti/ui/share/ShareViewModel.java
+++ app/src/main/java/com/example/kriti/ui/share/ShareViewModel.java
@@ -10,7 +10,7 @@ public class ShareViewModel extends ViewModel {
 
     public ShareViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is share fragment");
+        mText.setValue("This is Logout fragment");
     }
 
     public LiveData<String> getText() {
