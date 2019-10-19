4ad908d5ecaae38f28fdc3d2da6b4118151f6109
JatinSH <goeljatin28@gmail.com>
2019-10-19 13:25:45 +0530

    Added files

---
+++ app/src/main/java/com/example/kriti/CoursesActivity.java
@@ -0,0 +1,15 @@
+package com.example.kriti;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CoursesActivity extends AppCompatActivity {
+    private static final String TAG = "CoursesActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_courses);
+    }
+}
---
+++ app/src/main/java/com/example/kriti/ReadingMaterialActivity.java
@@ -0,0 +1,14 @@
+package com.example.kriti;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReadingMaterialActivity extends AppCompatActivity {
+    private static final String TAG = "ReadingMaterialActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material);
+    }
+}
